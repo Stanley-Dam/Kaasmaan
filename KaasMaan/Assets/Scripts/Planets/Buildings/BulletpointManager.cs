b6f35eee61505ef22d4e841bf2d92d3de4576ddd
Stanley-Dam <ditisstanley@gmail.com>
2019-05-08 22:17:06 +0200

    Initial Commit
    The first initial commit

---
+++ KaasMaan/Assets/Scripts/Planets/Buildings/BulletpointManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletpointManager : MonoBehaviour {
+
+    public Transform planetCenter;
+    public Transform planetSprite;
+    public GameObject bulletpointPrefab;
+    public Camera cam;
+
+    public int buildingsPerSize = 10;
+
+    private ArrayList bulletPoints = new ArrayList();
+
+    // Start is called before the first frame update
+    void Start() {
+        generateBulletpoints(planetSprite.localScale.x);
+    }
+
+    private void Update() {
+        if(Input.GetKey(KeyCode.A)) {
+            planetSprite.localScale += new Vector3(1, 1);
+            generateBulletpoints(planetSprite.localScale.x);
+            cam.orthographicSize++;
+        }
+    }
+
+    public void generateBulletpoints(float planetSize) {
+
+        updateBulletPoints(planetSize);
+
+        //Bulletpoint generate algorithm
+        for (int i = 0; i < (planetSize * buildingsPerSize); i++) {
+
+            float ang = i * (360 / (planetSize * buildingsPerSize));
+            float xPos = planetCenter.position.x + planetSize * 1.2f * Mathf.Sin(ang * Mathf.Deg2Rad);
+            float yPos = planetCenter.position.y + planetSize * 1.2f * Mathf.Cos(ang * Mathf.Deg2Rad);
+
+            //Check if the position is available
+            if (getBulletpointAtPosition(new Vector3(xPos, yPos, 0)) != null) continue;
+
+            Quaternion rotation = Quaternion.AngleAxis(ang, Vector3.forward);
+
+            GameObject currentObject = Instantiate(bulletpointPrefab, new Vector3(xPos, yPos, 0), rotation);
+            currentObject.transform.SetParent(this.transform, false);
+
+            //Add bullet point to the arraylist so we can get our information later on
+            bulletPoints.Add(currentObject);
+        }
+    }
+
+    private void updateBulletPoints(float planetSize) {
+
+        if (bulletPoints.Count > 0) {
+            //Move the existing bulletpoints
+            int index = 0;
+            foreach (GameObject currentObject in bulletPoints) {
+
+                float ang = index * (360 / (planetSize * buildingsPerSize));
+                float xPos = planetCenter.position.x + planetSize * 1.2f * Mathf.Sin(ang * Mathf.Deg2Rad);
+                float yPos = planetCenter.position.y + planetSize * 1.2f * Mathf.Cos(ang * Mathf.Deg2Rad);
+
+                Quaternion rotation = Quaternion.AngleAxis(ang, Vector3.forward);
+
+                currentObject.transform.position = new Vector3(xPos, yPos, 0);
+                currentObject.transform.rotation = rotation;
+
+                index++;
+            }
+        }
+
+    }
+
+    public GameObject getBulletpointAtPosition(Vector3 location) {
+        GameObject bulletPoint = null;
+
+        foreach (GameObject currentObject in bulletPoints) {
+            if (currentObject.transform.position.Equals(location))
+                bulletPoint = currentObject;
+        }
+
+        return bulletPoint;
+    }
+
+}
