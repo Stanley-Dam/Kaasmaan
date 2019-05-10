dd2baae4ab244127f41503883bd94cd43813c220
Stanley-Dam <ditisstanley@gmail.com>
2019-05-10 09:57:01 +0200

    Bulletpoint animations
    Added bulletpoint animations when you hover over the bulletpoints with the cursor.

---
+++ KaasMaan/Assets/Scripts/Planets/Buildings/Bulletpoints/BulletpointHover.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletpointHover : MonoBehaviour {
+
+    public float mouseHoverDistance = 0.5f;
+    public float normalBulletpointSize = 0.3f;
+    public float zoomedBulletpointSize = 1f;
+    public float zoomPerMicroSecond = 0.1f;
+
+    private bool isPlayerAnimation = false;
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+
+        //Check if the mouse is close enough to play the animation
+        if (Vector2.Distance(mousePosition, this.transform.position) < mouseHoverDistance) {
+            startAnimation(true);
+        } else if(this.transform.localScale.x > 0.3f) {
+            startAnimation(false);
+        }
+
+    }
+
+    private void startAnimation(bool zoomIn) {
+        if (!isPlayerAnimation && zoomIn) {
+            isPlayerAnimation = true;
+            StartCoroutine(doAnimation());
+        } else if(!isPlayerAnimation) {
+            isPlayerAnimation = true;
+            StartCoroutine(undoAnimation());
+        }
+    }
+
+    public IEnumerator doAnimation() {
+        while (this.transform.localScale.x < zoomedBulletpointSize) {
+
+            this.transform.localScale += new Vector3(zoomPerMicroSecond, zoomPerMicroSecond);
+            yield return new WaitForSeconds(0.01f);
+
+        }
+
+        StopCoroutine(doAnimation());
+        isPlayerAnimation = false;
+    }
+
+    public IEnumerator undoAnimation() {
+        while (this.transform.localScale.x >= normalBulletpointSize) {
+
+            this.transform.localScale -= new Vector3(zoomPerMicroSecond, zoomPerMicroSecond);
+            yield return new WaitForSeconds(0.01f);
+
+        }
+
+        StopCoroutine(undoAnimation());
+        isPlayerAnimation = false;
+    }
+
+}
---
+++ KaasMaan/Assets/Scripts/Planets/Buildings/Bulletpoints/BulletpointManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletpointManager : MonoBehaviour {
+
+    public Transform planetCenter;
+    public Transform planetSprite;
+    public GameObject bulletpointPrefab;
+    public Camera cam;
+
+    public int buildingsPerSize = 10;
+
+    private ArrayList bulletPoints = new ArrayList();
+
+    // Start is called before the first frame update
+    void Start() {
+        generateBulletpoints(planetSprite.localScale.x);
+    }
+
+    private void Update() {
+        //Testing the planet upscaling
+        if(Input.GetKeyDown(KeyCode.S)) {
+            planetSprite.localScale += new Vector3(1, 1);
+            generateBulletpoints(planetSprite.localScale.x);
+        }
+    }
+
+    public void generateBulletpoints(float planetSize) {
+
+        updateBulletPoints(planetSize);
+        ArrayList newBulletPoints = new ArrayList();
+
+        //Bulletpoint generate algorithm
+        for (int i = bulletPoints.Count; i < (buildingsPerSize + bulletPoints.Count); i++) {
+            float ang = i * 360 / (buildingsPerSize + bulletPoints.Count);
+            float xPos = planetCenter.localPosition.x + planetSize * 1.2f * Mathf.Sin(ang * Mathf.Deg2Rad);
+            float yPos = planetCenter.localPosition.y + planetSize * 1.2f * Mathf.Cos(ang * Mathf.Deg2Rad);
+
+            //Check if the position is available
+            if (getBulletpointAtPosition(new Vector3(xPos, yPos, 0)) != null) continue;
+
+            Quaternion rotation = Quaternion.AngleAxis(ang, Vector3.forward);
+
+            GameObject currentObject = Instantiate(bulletpointPrefab, new Vector3(xPos, yPos, 0), rotation);
+            currentObject.transform.SetParent(this.transform, false);
+
+            //Add bullet point to the arraylist so we can get our information later on
+            newBulletPoints.Add(currentObject);
+        }
+
+        foreach (GameObject currentObject in newBulletPoints) {
+            bulletPoints.Add(currentObject);
+        }
+    }
+
+    private void updateBulletPoints(float planetSize) {
+
+        if (bulletPoints.Count > 0) {
+            //Move the existing bulletpoints
+            int index = 0;
+            foreach (GameObject currentObject in bulletPoints) {
+
+                float ang = index * 360 / (buildingsPerSize + bulletPoints.Count);
+                float xPos = planetCenter.localPosition.x + planetSize * 1.2f * Mathf.Sin(ang * Mathf.Deg2Rad);
+                float yPos = planetCenter.localPosition.y + planetSize * 1.2f * Mathf.Cos(ang * Mathf.Deg2Rad);
+
+                Quaternion rotation = Quaternion.AngleAxis(ang, Vector3.forward);
+
+                currentObject.transform.localPosition = new Vector3(xPos, yPos, 0);
+                currentObject.transform.localRotation = rotation;
+
+                index++;
+            }
+        }
+
+    }
+
+    public GameObject getBulletpointAtPosition(Vector3 location) {
+        GameObject bulletPoint = null;
+
+        foreach (GameObject currentObject in bulletPoints) {
+            if (currentObject.transform.position.Equals(location))
+                bulletPoint = currentObject;
+        }
+
+        return bulletPoint;
+    }
+
+}
